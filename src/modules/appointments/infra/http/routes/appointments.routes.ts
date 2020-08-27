9b4c765480677777d6db828b5f333cf49a744a5b
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2020-08-27 18:03:15 -0300

    Dependency Inversion Principle

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,28 +1,28 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
-import AppointmentRepository from '@modules/appointments/repositories/appointmentRepository';
+import AppointmentRepository from '@modules/appointments/infra/typeorm/repositories/appointmentRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentRouter = Router();
 
+const appointmentRepository = new AppointmentRepository();
+
 appointmentRouter.use(ensureAuthenticated);
 
-appointmentRouter.get('/', async (request, response) => {
-  const appointmentRepository = getCustomRepository(AppointmentRepository);
+/* appointmentRouter.get('/', async (request, response) => {
   const appointments = await appointmentRepository.find();
 
   return response.json(appointments);
-});
+}); */
 
 appointmentRouter.post('/', async (request, response) => {
   const { provider_id, date } = request.body;
 
   const parsedDate = parseISO(date);
 
-  const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService(appointmentRepository);
 
   const appointment = await createAppointment.execute({
     date: parsedDate,
--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,33 +1,29 @@
 import { startOfHour } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
 import AppError from '@shared/errors/AppError';
 
 import Appointment from '../infra/typeorm/entities/appointment';
-import AppointmentRepository from '../repositories/appointmentRepository';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
-interface IAppointmentRequest {
+interface IRequest {
   provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({
-    date,
-    provider_id,
-  }: IAppointmentRequest): Promise<Appointment> {
+  constructor(private appointmentRepository: IAppointmentsRepository) {}
+
+  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    const appointmentRepository = getCustomRepository(AppointmentRepository);
-
-    const appointmentInSameDate = await appointmentRepository.findByDate(
+    const appointmentInSameDate = await this.appointmentRepository.findByDate(
       appointmentDate,
     );
     if (appointmentInSameDate) {
       throw new AppError('This appointment is already booked.');
     }
 
-    const appointment = await appointmentRepository.create({
+    const appointment = await this.appointmentRepository.create({
       provider_id,
       date: appointmentDate,
     });
