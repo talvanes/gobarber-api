327de47beac8022db16896042cec5e0d11b5c9ff
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2020-09-11 13:06:11 -0300

    Atualização do perfil

---
+++ src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,72 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import { isThisHour } from 'date-fns';
+import IUserRepository from '../repositories/IUserRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+import User from '../infra/typeorm/entities/user';
+
+interface IRequest {
+  user_id: string;
+  name: string;
+  email: string;
+  old_password?: string;
+  password?: string;
+}
+
+@injectable()
+class UpdateProfileService {
+  constructor(
+    @inject('UserRepository')
+    private usersRepository: IUserRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({
+    user_id,
+    name,
+    email,
+    password,
+    old_password,
+  }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+    if (!user) {
+      throw new AppError('User not found.');
+    }
+
+    const checkAnotherEmail = await this.usersRepository.findByEmail(email);
+    if (checkAnotherEmail && checkAnotherEmail.id !== user_id) {
+      throw new AppError('Email already in use.');
+    }
+
+    user.name = name;
+    user.email = email;
+
+    if (password && !old_password) {
+      throw new AppError(
+        'You must inform the current password in order to update password',
+      );
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await this.hashProvider.compareHash(
+        old_password,
+        user.password,
+      );
+
+      if (!checkOldPassword) {
+        throw new AppError('The current password does not match.');
+      }
+
+      user.password = await this.hashProvider.generateHash(password);
+    }
+
+    return this.usersRepository.save(user);
+  }
+}
+
+export default UpdateProfileService;
