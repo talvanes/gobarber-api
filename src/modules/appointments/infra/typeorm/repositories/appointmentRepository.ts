6df547fe3c731ab1ca0282da3220a00cc70a3040
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2020-10-03 14:09:54 -0300

    Serialização do cache

--- src/modules/appointments/infra/typeorm/repositories/appointmentRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/appointmentRepository.ts
@@ -40,7 +40,6 @@ class AppointmentRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`,
         ),
       },
-      relations: ['user'],
     });
 
     return appointments;
@@ -63,6 +62,7 @@ class AppointmentRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
