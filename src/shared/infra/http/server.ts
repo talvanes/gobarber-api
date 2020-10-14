be4a0feda7ccebbe6c39522e8dc35bac2e41f19e
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2020-10-14 12:47:39 -0300

    Nível 04: middleware rate-limiter só não vai se aplicar a requisições a arquivos estáticos (static file requests)

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -18,10 +18,11 @@ import rateLimiter from './middlewares/rateLimiter';
 
 const app = express();
 
+app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(rateLimiter);
+
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
 app.use(errors());
